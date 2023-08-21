8191b79b5884ec95f2d76c9125b8def060e27452
Ankit Saurabh <ankitsaurabh57@gmail.com>
2023-08-21 14:09:09 +0530

    NIKE CLONE

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './Sections';
+import Nav from './Components/Nav';
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav/>
+      <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+      <section className='padding'><PopularProducts/></section>
+      <section className='padding'><SuperQuality/></section>
+      <section className='padding'><Services/></section>
+      <section className='padding'><SpecialOffer/></section>
+      <section className='padding bg-pale-blue'><CustomerReviews/></section>
+      <section className='padding padding-x sm:py-32 py-16 w-full'><Subscribe/></section>
+      <section className='padding bg-black padding-x padding-t pb-8'><Footer/></section>
+    </main>
+  )
+}
+
+export default App
